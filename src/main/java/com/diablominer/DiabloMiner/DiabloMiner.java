0a9017a88a622baea51b4d0de24837fabc381f5c
Ang Iong Chun <angiongchun@gmail.com>
2011-07-20 16:15:36 +0800

    use round-robin for load balance

--- src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
+++ src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
@@ -87,6 +87,7 @@ class DiabloMiner {
 
   NetworkState[] networkStates;
   int networkStatesCount;
+  int networkStateIndex = 0;
   Proxy proxy = null;
   int getWorkRefresh = 5000;
   final ObjectMapper mapper = new ObjectMapper();
@@ -1475,10 +1476,11 @@ class DiabloMiner {
         boolean rollNTime = false;
         int rolledNTime = 0;
 
-        NetworkState networkState = networkStates[new Random().nextInt(networkStatesCount)];
+        NetworkState networkState;
         AtomicReference<GetWorkItem> getWorkIncoming = new AtomicReference<GetWorkItem>(null);
 
         GetWorkParser() {
+	  networkState = networkStates[(networkStateIndex++) % networkStatesCount];
           getWork(false);
         }
 
