dd4d25d5bf156a75721b982f2f1601bb48617595
Ang Iong Chun <angiongchun@gmail.com>
2011-08-06 21:11:45 +0800

    remove debug message

--- src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
+++ src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
@@ -1381,7 +1381,6 @@ class DiabloMiner {
             default:
               nwsIdx = (int)(networkStatesCount * Math.random());
           }
-          edebug("new GetWorkParser: nws=" + nwsIdx);
           networkState = networkStates[nwsIdx];
           getWork(false);
         }
